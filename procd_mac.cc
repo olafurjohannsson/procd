70cb58c4f3efbc3f2a3a8051d5631fa307ededc3
Ólafur Jóhannsson <olafurjohannss@gmail.com>
2017-03-15 21:57:34 +0000

    macOS procd

---
+++ procd_mac.cc
@@ -0,0 +1,16 @@
+
+#include <mach/mach.h>
+#include <mach/mach_vm.h>
+#include <mach/shared_region.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <sys/sysctl.h>
+
+
+namespace procd {
+    
+    namespace {
+        
+    }
+    
+}
