ad829c94bde46c91165c0e2caa3c0e75faaf0449
Ólafur Jóhannsson <olafurjohannss@gmail.com>
2017-05-07 20:57:52 +0000

    mem

--- memory/shared_memory_test.cpp
+++ memory/shared_memory_test.cpp
@@ -1,24 +1,51 @@
 
 
-
 #include "shared_memory.h"
 
+void *start_callback(void)
+{
+    void *p;
 
-int main() {
+    return p;
+}
+void *stop_callback(void)
+{
+    void *p;
 
+    return p;
+}
+struct meta
+{
+    enum
+    {
+        running,
+        stopped,
+        error,
+        cancelled
+    } state = meta::running;
+    union {
+        (fn_start *) = start_callback;
+    } callback;
+} metadata;
+int main()
+{
+
+    if (metadata.state == meta::running)
+    {
+        print("started");
+    }
+
+    return 0;
     root::shared_memory shm;
-
 
     if (shm.create("", 1024))
     {
         print("works");
     }
-    else 
+    else
     {
         print("did not work");
     }
 
-
     return 0;
-
 }
